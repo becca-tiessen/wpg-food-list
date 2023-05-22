34da5f2ef270b634dc4d809bbefc6fac397df6a7
Rebecca Tiessen <becca.tiessen@gmail.com>
2023-05-22 12:53:01 -0500

    Added a nav menu

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,27 @@
 import React from "react";
-import {Box, Container, List, ListItem, ListItemText} from '@mui/material';
+import {Container, List, ListItem, ListItemText, Rating} from '@mui/material';
+import NavMenu from './components/NavMenu.tsx'
 
 const App = () =>{
     return (
         <>
-        <Container maxWidth="sm">
-        <h1>
-            welcome to winnipeg's food list.
-        </h1>
-        <List>
-          <ListItem disablePadding>
-              <ListItemText primary="Sous Sol" />
-          </ListItem>
-          <ListItem disablePadding>
-              <ListItemText primary="Deer + Almond" />
-          </ListItem>
-        </List>
-        </Container>
+        <NavMenu>
+            <Container maxWidth="sm">
+            <h1>
+                welcome to winnipeg's food list.
+            </h1>
+            <List>
+            <ListItem disablePadding>
+                <ListItemText primary="Deer + Almond" />
+                <Rating name="half-rating" value={5.0} precision={0.5} />
+            </ListItem>
+            <ListItem disablePadding>
+                <ListItemText primary="Sous Sol" />
+                <Rating name="half-rating" value={4.5} precision={0.5} />
+            </ListItem>
+            </List>
+            </Container>
+        </NavMenu>
         </>
     )
 }
